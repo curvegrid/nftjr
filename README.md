d9b8d8cc56bbd6a5571376b273c590e626a1cf09
William Metcalfe <william.metcalfe@gmail.com>
2021-03-21 17:23:35 +0900

    Add axios to talk to MultiBaas directly, fetch family on login (but just log to console for now)

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ cd contracts
 yarn install
 truffle migrate --reset
 ```
+## Frontend Setup
+
+```
+cd frontend
+```
+- Copy `./axios_with_config.template.js` to `./axios_with_config.js`
+- In `axios_with_config.js`
+  - Update the `apiKey` variable with a valid MultiBaas API key
+  - Update the `baseURL` variable with the correct hostname
+```
+yarn install
+yarn serve
+```
+- Configure CORS in MultiBaas > Admin > CORS Domains to include the domain and port of server
--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -9,6 +9,7 @@
 <script>
   import Dialogue from '../components/Dialogue';
   import ethers from '../store/ethers/ethersConnect';
+  import axios from '../../axios_with_config';
 
   export default {
     name: 'Login',
@@ -18,12 +19,33 @@
     data: () => ({
     }),
     methods: {
+      async getFamily(address) {
+        const body = {
+          args: [
+            address,
+            0, // offset
+            1  // limit
+          ],
+          from: address
+        }
+        const response = await axios.post('/families/contracts/families/methods/getFamilyMemberships',body)
+        const families = response.data.result.output
+        if (families && families.length > 0) {
+          return families[0]
+        }
+        return null
+      },
       async login() {
         console.log('login')
         window.ethereum.request({ method: 'eth_requestAccounts' })
         this.$store.dispatch('ethers/connect')
         const addr = await ethers.getWalletAddress()
-        console.log(addr);
+        const family = await this.getFamily(addr)
+        if (family !== null) {
+          console.log('Yea: ',family)
+        } else {
+          console.log('Nay...')
+        }
       }
     }
   }
