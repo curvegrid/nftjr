b78befa89bc97e9a799d003d972c1a02e8c55532
William Metcalfe <william.metcalfe@gmail.com>
2021-03-21 12:28:32 +0900

    Add Vuex (for ethers.js)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import vuetify from './plugins/vuetify';
 import VueRouter from 'vue-router'
 
 import Login from './pages/Login'
+import store from './store'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
@@ -19,5 +20,6 @@ const router = new VueRouter({
 new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
